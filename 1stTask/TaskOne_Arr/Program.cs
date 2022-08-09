7de0119aef8149e3f425eb1b47c0fd553ff48231
OmarElshamy22 <105892198+OmarElshamy22@users.noreply.github.com>
2022-08-09 15:51:56 +0200

    fix hidden folders

---
+++ 1stTask/TaskOne_Arr/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskOne_Arr
+{
+    class Program
+    {
+         static int[] counter ;
+        static char[] character  ;
+
+        static void dic(char []small, String big)
+        {
+            for (int i = 0; i < small.Length; i++)
+            {
+                        character[i] = char.ToUpper(small[i]);
+                for (int j = 0; j < big.Length; j++)
+                {
+                    if (char.ToUpper(small[i]) == big.ToUpper().ToCharArray()[j])
+                    {
+                        counter[i] += 1;
+                        
+                    }
+                    
+
+                }
+
+            }
+
+        }
+      
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please Enter First Word");
+            string Word = Console.ReadLine(); ;
+            Console.WriteLine("Enter Secound Word");
+            string OtherWord = Console.ReadLine(); ;
+            if (OtherWord.Length> Word.Length)
+            {
+                counter = new int[Word.Distinct().Count()];
+                character= new char[Word.Distinct().Count()];
+                dic(Word.Distinct().ToArray(), OtherWord);
+            }
+            else
+            {
+                counter = new int[OtherWord.Distinct().Count()];
+                character = new char[OtherWord.Distinct().Count()];
+                dic(OtherWord.Distinct().ToArray(), Word);
+            }
+
+            for (int i = 0; i < counter.Length; i++)
+            {
+                Console.WriteLine(character[i]+": "+ counter[i]);
+
+
+
+            }
+
+        }
+    }
+}
